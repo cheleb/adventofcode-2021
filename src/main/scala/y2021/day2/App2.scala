499c642b24d61e0ef1299a0a48bc3aa6547298fd
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2021-12-02 19:08:54 +0100

    Day 2

---
+++ src/main/scala/y2021/day2/App2.scala
@@ -0,0 +1,40 @@
+package y2021.day2
+
+import zio._
+import zio.Console._
+import zio.stream.ZStream
+
+import zio.stream.ZPipeline
+
+object App2 extends App {
+
+  val pattern = """(\w+)\s+(\d+)""".r
+
+  case class Pos(x: Int, y: Int, aim: Int)
+
+  val program = for {
+
+    pos <- ZStream
+      .fromInputStream(
+        getClass().getResourceAsStream("/y2021/day2/input.txt")
+      )
+      .via(ZPipeline.utf8Decode)
+      .via(ZPipeline.splitLines)
+      .collect { case pattern(move, value) => (move, value.toInt) }
+      .fold(Pos(0, 0, 0)) { case (pos, (move, value)) =>
+        println(s"$move $value")
+        move match {
+          case "forward" =>
+            pos.copy(x = pos.x + value, y = pos.y + pos.aim * value)
+          case "up"   => pos.copy(aim = pos.aim - value)
+          case "down" => pos.copy(aim = pos.aim + value)
+        }
+      }
+
+    _ <- putStrLn(s"$pos: ${pos.x * pos.y}")
+  } yield ()
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    program.exitCode
+
+}
